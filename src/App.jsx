6b9112d785c8ae4be205e2bba47a17447560bc82
Fernando Valdes <140764694+Martin-Valdes@users.noreply.github.com>
2024-01-30 08:57:21 -0300

    conf git-hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-// import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
-// import HomeContainer from "./components/HomeContainer/HomeContainer"
-// import ContactoContainer from "./components/ContactoContainer/ContactoContainer"
-// import FinanciacionContainer from "./components/FinanciacionContainer/FinanciacionContainer"
-// import NavBar from "./components/NavBar/NavBar"
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import HomeContainer from "./components/HomeContainer/HomeContainer"
+import ContactoContainer from "./components/ContactoContainer/ContactoContainer"
+import FinanciacionContainer from "./components/FinanciacionContainer/FinanciacionContainer"
+import NavBar from "./components/NavBar/NavBar"
 import { BrowserRouter,Routes, Route, Navigate } from "react-router-dom"
 
 
@@ -14,9 +14,9 @@ function App() {
         <NavBar/>
         <Routes>
         <Route path="/" element = {<HomeContainer/>}/>
-          {/* <Route path="autos/autos" element ={<ItemListContainer/>} /> */}
+          <Route path="autos/autos" element ={<ItemListContainer/>} />
           <Route path="financiacion" element = {<FinanciacionContainer/>}/>
-          {/* <Route path="contacto" element = {<ContactoContainer/>}/> */}
+          <Route path="contacto" element = {<ContactoContainer/>}/>
         </Routes>
       </div>
     </BrowserRouter>
