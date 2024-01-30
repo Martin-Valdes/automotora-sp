3d97dcb7b60641d4398a24327de64e6ac8775358
Fernando Valdes <140764694+Martin-Valdes@users.noreply.github.com>
2024-01-30 07:26:53 -0300

    conf git-hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
-import HomeContainer from "./components/HomeContainer/HomeContainer"
+// import HomeContainer from "./components/HomeContainer/HomeContainer"
 // import ContactoContainer from "./components/ContactoContainer/ContactoContainer"
-import FinanciacionContainer from "./components/FinanciacionContainer/FinanciacionContainer"
+// import FinanciacionContainer from "./components/FinanciacionContainer/FinanciacionContainer"
 import NavBar from "./components/NavBar/NavBar"
 import { BrowserRouter,Routes, Route, Navigate } from "react-router-dom"
 
